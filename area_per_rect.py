8bfe6e54d3d8bd9ce720cd1622c1e73220842b12
Mikhail Ibrahim <Mikhail.ibrahim@stu.ocsb.ca>
2025-02-18 03:21:50 +0000

    Update area and perimeter calculations in area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Mikhail Ibrahim
+# Date: Feb 11, 2024
+# Description: A Python program that calculates the area and perimeter of a rectangle.
+
+length = 10
+width = 4
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("For a rectangle with length {}cm and width {}cm:".format(length, width))
+print("The area is {} square centimeters.".format(area))
+print("The perimeter is {} centimeters.".format(perimeter))
